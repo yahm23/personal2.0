39bf6abd62176ae2ee4111766c80065f5743d3a1
yahm23 <yahmed1996a@gmail.com>
2020-11-12 16:50:25 +0000

    stripping down and refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import { useState,useEffect } from 'react';
 import './App.css';
 import Loader from './components/loader';
+import MainPageContent from './components/mainpageContent';
 
 function App() {
   const[loadingSwitch,setLoader]=useState(true)
   
   useEffect(() => {
-    let myVar = setTimeout(function(){ setLoader(false) }, 4000);
-    // setTimeout(setLoader(false), 7000);
-    
+    let myVar = setTimeout(function(){ setLoader(false) }, 3000);
   },[] )
 
 
   return (
     <div className="App">
       {loadingSwitch? 
-        <Loader></Loader>:
-        <div>
-          <h1 className="test fade-in">Application shit.</h1>
-        </div>
-        }
+        <Loader/>:
+        <MainPageContent/>
+      }
     </div>
   );
 }
---
+++ src/components/mainpageContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainPageContent() {
+    return (
+        <div>
+          <h1 className="test fade-in">Application shit.</h1>
+        </div>
+    )
+}
