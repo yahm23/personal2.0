0d6bf2ce9b00924c9fb9f05ba02f5949cc972d8f
yahm23 <yahmed1996a@gmail.com>
2020-11-12 16:21:32 +0000

    added laoder with timout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import Loader from './components/loader';
 
 function App() {
+  const[loadingSwitch,setLoader]=useState(true)
+  
+  useEffect(() => {
+    let myVar = setTimeout(function(){ setLoader(false) }, 5000);
+    // setTimeout(setLoader(false), 7000);
+    
+  },[] )
+
+
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
+      {loadingSwitch? 
+        <Loader></Loader>:
+        <div>
+          <h1 className="test">Application shit.</h1>
+        </div>
+        }
     </div>
   );
 }
