83de68954eb47128dec9aed5378b7f99781615d3
yahm23 <yahmed1996a@gmail.com>
2020-11-13 16:48:24 +0000

    arrangment of logo comps and navbar added, redone

--- src/components/functionalComponents/navbar.js
+++ src/components/functionalComponents/navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import AnimatedLogo from './animLogo'
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            <h1 className="navTitles">About</h1>
-            <h1 className="navTitles">Skills</h1>
-            <h1 className="navTitles">Porfolio</h1>
-        </div>
+        <div className="topBar">
+            <AnimatedLogo/>
+            <div className="navbar">
+                <h1 className="navTitles">About</h1>
+                <h1 className="navTitles">Skills</h1>
+                <h1 className="navTitles">Porfolio</h1>
+            </div>
+          </div>
     )
 }
--- src/components/pageComponents/mainpageContent.js
+++ src/components/pageComponents/mainpageContent.js
@@ -5,10 +5,7 @@ import Navbar from '../functionalComponents/navbar'
 export default function MainPageContent() {
     return (
         <div className="mainContentBody">
-          <div className="topBar">
-            <AnimatedLogo/>
             <Navbar/>
-          </div>
           <h1  className="test" >MAIN PAGE SHI</h1>
         </div>
     )
