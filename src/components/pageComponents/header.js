d97a6339cb56133276ab3e7c70519640cc37efaa
Yousef <yahmed1996a@gmail.com>
2020-11-15 11:31:39 +0000

    added new colourway that can be easy changed with sass set up

--- src/components/pageComponents/header.js
+++ src/components/pageComponents/header.js
@@ -1,9 +1,20 @@
-import React from 'react'
 
-export default function Header() {
+import React, { Component } from 'react';
+
+
+
+
+const Header=(props)=> {
+
     return (
         <div>
-            
+            <div className="headerTitle">
+                <h1 className="developer">Software Developer </h1>
+                <h2 className= "developer commentedOut">//Bioengineer</h2>
+            </div>
         </div>
-    )
+    );
 }
+
+
+export default Header;
